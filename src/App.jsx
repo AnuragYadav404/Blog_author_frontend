5cdf44ec64f64af01556e5e7f42dc7045a021952
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-11-01 12:10:44 +0530

    Set up for tailwind css and App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  /* app will have two things
+    1. The side navigation bar
+    2. The main outlet
+  */
+  return (
+    <>
+      <div id="navigation_sidebar" className="text-3xl font-bold underline">
+        <h1>This is the app page</h1>
+      </div>
+      <div id="content_outlet">
+        <h2>This is content outlet</h2>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello Moto!</h1>,
+    element: <App />,
   },
 ]);
 
